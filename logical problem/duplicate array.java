8bc025ba31c84f81a0e5150204c563db7d6c7777
haripriya1807 <haripriya18072002@gmail.com>
2023-07-11 16:17:24 +0530

    array

---
+++ logical problem/duplicate array.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+
+
+// find the duplicate in array.
+public class Main {
+    public static int[] main(int[] arr) {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        int[] uniqueArr = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            uniqueArr[index++] = num;
+        }
+
+        return uniqueArr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 6, 6};
+        int[] uniqueArr = main(arr);
+        System.out.println("given number is duplicate : " + Arrays.toString(arr));
+        System.out.println("Array number is  duplicates: " + Arrays.toString(uniqueArr));
+    }
+}
+
